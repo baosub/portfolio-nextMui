ec76f8a9e159e18e9c9705f41168667d14c8f928
Baldo <barrosoub@protonmail.com>
2024-09-17 07:56:25 -0600

    the projecs section was modified

--- app/bodyComponents/Bprojects.tsx
+++ app/bodyComponents/Bprojects.tsx
@@ -14,34 +14,64 @@ interface Project {
 
 // Datos de ejemplo de los proyectos
 const projects: Project[] = [
+
   {
-    title: 'Mexican Population',
-    description: 'This app shows fetched data from the official Government API (INEGI), the charts are made with the library recharts, and dark mode was applied.',
+    title: 'AI-Powered Nutrition Assistant Chatbot',
+    description: `This project is an AI-powered Nutrition Assistant Chatbot that leverages OpenAI’s GPT-4 model to provide personalized
+     nutritional guidance. It calculates Body Mass Index (BMI), daily nutritional requirements, and macronutrient distribution to help users 
+     efficiently organize their diets and achieve their health goals. The chatbot integrates artificial intelligence to perform accurate 
+     calculations and offer tailored advice. Developed with Next.js 14 (App Router), React, and TypeScript, it features Material UI for styling 
+     and includes a dark mode option for enhanced user experience.`,
+    imageUrl: '/nutri.png', // Ruta de la imagen
+    url: 'https://nutri-app-five.vercel.app/',
+  },
+  {
+    title: 'Population Data Visualization Dashboard',
+    description: `This project is a data visualization dashboard that fetches population data from the official INEGI API, a government
+     agency responsible for national statistics and geography in Mexico. It features interactive charts, including visualizations of population
+      by state, gender distribution, and the relationship between population and housing by state. Built using React, JavaScript, and Material UI, 
+      the app utilizes Axios for data fetching and Recharts for displaying the graphs. It also includes a dark mode option for user preference.`,
     imageUrl: '/mxpop.png', // Ruta de la imagen
     url: 'https://cerulean-cascaron-35df7e.netlify.app/',
   },
+
+
   {
-    title: 'MUI components',
-    description: 'This is a collection of Material UI componens, using client side rendering and routes with react router dom',
+    title: 'Single Page Application with Client-Side Rendering',
+    description: `This project is a Single Page Application (SPA) built with React and JavaScript, utilizing client-side rendering. 
+    It features routing with React Router DOM, including active links to display the current route, and incorporates examples of managing local 
+    state with the useState hook. The app also follows responsive web design principles for optimal display across devices. Styling is implemented 
+    using Material UI.`,
     imageUrl: '/muicomps.png', // Ruta de la imagen
     url: 'https://spiffy-moonbeam-602c69.netlify.app/',
   },
   {
-    title: 'Gradients Generator',
-    description: 'This app generates gradients ready to be used in React.',
+    title: 'CSS Gradient Generator',
+    description: `This project is a tool for generating CSS gradients through an intuitive interface. Users can create four types of gradients: 
+    two-color linear gradients with customizable angle, three-color linear gradients, two-color radial gradients, and three-color radial gradients.
+     The application leverages input fields for color selection and angle adjustment, dynamically updating the gradient preview using local state 
+     management with the useState hook. Built with React, JavaScript, and styled using Material UI, it allows users to generate ready-to-use 
+     gradient code for their designs.`,
     imageUrl: '/gradients.png', // Ruta de la imagen
     url: 'https://warm-smakager-5e994b.netlify.app/',
   },
   {
-    title: 'Small business site',
-    description: 'This is a design of the website of a small business',
+    title: 'Small Business Website Layout',
+    description: `This project is a layout design for a small business website, showcasing its products and services. Built with React, 
+    JavaScript, and styled using Material UI, the focus was on creating a clean and organized structure, highlighting key offerings 
+    in a user-friendly manner. As one of my first web development projects, it demonstrates fundamental skills in responsive design and 
+    component-based architecture.`,
     imageUrl: '/alumi.png', // Ruta de la imagen
     url: 'https://astounding-profiterole-356963.netlify.app/',
   },
 
   {
-    title: 'Portfolio',
-    description: 'This is a template of portfolio that uses github API, dev.to API, add google analytics and adds a lot of styles and themes',
+    title: 'Forked Portfolio Template with API Integrations',
+    description: `This project is a forked portfolio template that integrates several APIs, including GitHub, Dev.to, and Medium, with links 
+    to personal social media profiles such as LinkedIn, Twitter, and Instagram. It features over 20 themes and is fully responsive. I focused on 
+    forking the repository, customizing the content with my personal information, and managing the project using Git. Additionally, I integrated 
+    Google Analytics for traffic monitoring and deployed the site on GitHub Pages. This project highlights my skills in repository management, 
+    customization, and deployment.`,
     imageUrl: '/portfolio.png', // Ruta de la imagen
     url: 'https://baosub.github.io/',
   },
